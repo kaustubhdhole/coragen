b015f2d41a506f81ae3b081daf717c9aa23c6592
Atharva Negi <atharvanegi232@gmail.com>
2025-03-29 02:47:26 -0400

    Add docs folder for GitHub Pages deployment

---
+++ docs/precache-manifest.466d5d78689c823794869e7c11eba9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b6816fee46ccee73522bf00868fde5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e4302769f51f365011",
+    "url": "/static/css/main.e6a2e46f.chunk.css"
+  },
+  {
+    "revision": "a3dcab0030ad528a309d",
+    "url": "/static/js/2.259d21fe.chunk.js"
+  },
+  {
+    "revision": "dad19ae7863f91a26c6cc2a44772ab69",
+    "url": "/static/js/2.259d21fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8588b8771292bb19e6",
+    "url": "/static/js/3.3007e8cc.chunk.js"
+  },
+  {
+    "revision": "30e4302769f51f365011",
+    "url": "/static/js/main.62fb1273.chunk.js"
+  },
+  {
+    "revision": "7776a0f14555e9496b18",
+    "url": "/static/js/runtime-main.dae92097.js"
+  }
+]);
